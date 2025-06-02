29d0acc624cd01a77f37a40cf2512bf335a56a11
Lejo Flores <lejoflores@boisestate.edu>
2025-06-02 13:20:51 -0600

    added non-notebook file to illustrate functions

---
+++ Unit04-Geospatial-Data/lulc_in_zrange.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+def lulc_in_zrange(zgrid, lulcgrid, zmin, zmax, lulckeys):
+    
+    
+    # Get the land cover grid within the input elevation range
+    
+    # Get the histogram
+    
+    
+    return nbins, lulcfrac
